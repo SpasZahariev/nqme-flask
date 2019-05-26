49cac16b80be1fcc448222495c2eea4703639fdf
SpasZahariev <spaszahariev54@gmail.com>
2019-05-26 21:44:05 +0100

    adding updated versions to libraries

--- configExample.py
+++ configExample.py
@@ -1,6 +1,5 @@
 MONGODB_CONFIG = {
     # 'URL': 'mongodb+srv://CloudApp3207:<PASSWORD>@cluster0-x7jkw.gcp.mongodb.net/test?retryWrites=true'
-    'URL': 'mongodb+srv://spaszahariev54:katinar4e@nqme-pe1y7.mongodb.net/test?retryWrites=true'
 }
 
 TOKEN_KEYS = {
